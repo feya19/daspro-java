0c6775d6bef91bfdfd5fd543e9cf688687432ee7
miicheelll <doranimichelle@gmail.com>
2023-09-26 07:58:04 +0700

    add transaksi

--- Kantin.java
+++ Kantin.java
@@ -17,6 +17,26 @@ public class Kantin {
             System.exit(0);
         }
 
+        String namaBarang;
+        float diskon, ppn;
+        double harga, total, hargaDiskon, totalDiskon, totalPpn;
+        
+        System.out.print("Masukkan nama barang : ");
+        namaBarang = input.next();
+        System.out.print("Masukkan harga barang : ");
+        harga = input.nextDouble();
+        System.out.print("Masukkan Diskon (%): ");
+        diskon = input.nextFloat() / 100;
+        System.out.print("Masukkan PPN (%): ");
+        ppn = input.nextFloat() / 100;
+
+        hargaDiskon = harga * diskon;
+        totalDiskon = harga - hargaDiskon;
+        totalPpn = totalDiskon * ppn;
+        total = totalDiskon + totalPpn;
+
+        System.out.println("Total transaksi anda " + String.format("%.2f", total));
+
         input.close();
     }
 }
